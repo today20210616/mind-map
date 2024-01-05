ef77283a9f8003a0d44445ac7a8b13596e326526
Todayy <today20210616@gmail.com>
2024-01-05 14:13:17 +0800

    Modify: disable log

--- simple-mind-map/src/core/event/Event.js
+++ simple-mind-map/src/core/event/Event.js
@@ -146,11 +146,11 @@ class Event extends EventEmitter {
     if (e.deltaX < 0) dirs.push(CONSTANTS.DIR.LEFT)
     if (e.deltaX > 0) dirs.push(CONSTANTS.DIR.RIGHT)
     // 判断是否是触控板
-    console.log("ontouchstart: " + ('ontouchstart' in window))
-    console.log("maxTouchPoints: " + navigator.maxTouchPoints)
-    console.log("msMaxTouchPoints: " + navigator.msMaxtouchPoints)
-    console.log("window.PointerEvent: " + window.PointerEvent)
-    console.log("window.MSPointerEvent: " + window.MSPointerEvent)
+    // console.log("ontouchstart: " + ('ontouchstart' in window))
+    // console.log("maxTouchPoints: " + navigator.maxTouchPoints)
+    // console.log("msMaxTouchPoints: " + navigator.msMaxtouchPoints)
+    // console.log("window.PointerEvent: " + window.PointerEvent)
+    // console.log("window.MSPointerEvent: " + window.MSPointerEvent)
 
 
     let isTouchPad = false
